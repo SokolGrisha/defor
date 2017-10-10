688929350a817452fb6f786ce4cf0ebea6d7ec9f
azbang <azbango5@gmail.com>
2017-10-10 15:42:12 +0300

    hot fix

--- dev/EthAPI.js
+++ dev/EthAPI.js
@@ -1,3 +1,5 @@
+var Buffer = require('buffer').Buffer;
+
 module.exports = function EthAPI(to_address, abi) {
 	this.web3 = new Web3();
 	this.web3.setProvider(new this.web3.providers.HttpProvider("https://ropsten.infura.io"));
@@ -15,7 +17,7 @@ module.exports = function EthAPI(to_address, abi) {
 	}
 
 	this.add = function(props) {
-		this.private_key = buffer.Buffer.from(props.key, 'hex');
+		this.private_key = Buffer.from(props.key, 'hex');
 
 		this.web3.eth.getTransactionCount(props.fromAdress).then((current_nonce) => {
 			let payloadData = this.contract.methods.add(props.addHash).encodeABI();
